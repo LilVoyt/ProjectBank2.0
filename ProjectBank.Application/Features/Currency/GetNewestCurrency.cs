1c2e7269dbc1d78472ecd206c1fd2e310bcb3399
Voyt <olegvoyt6@gmail.com>
2024-10-19 12:41:31 +0300

    Currency api added(demo)

---
+++ ProjectBank.Application/Features/Currency/GetNewestCurrency.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBank.BusinessLogic.Features.Currency
+{
+    public class GetNewestCurrency : IGetNewestCurrency
+    {
+        public string GetFromApi()
+        {
+            var client = new RestClient("https://api.currencyapi.com/v3/latest");
+
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("apikey", "cur_live_fC3YUWtrfS6J60HWoPMxOfliB5RFfglFc0gxEznJ");
+            IRestResponse response = client.Execute(request);
+            var jsonResponse = JObject.Parse(response.Content);
+            var usdToUahRate = jsonResponse["data"]["RUB"]["value"].ToObject<decimal>();
+
+            Console.WriteLine(response.Content);
+            return usdToUahRate.ToString();
+        }
+    }
+}
--- ProjectBank.Presentation/Controllers/CardController.cs
+++ ProjectBank.Presentation/Controllers/CardController.cs
@@ -1,18 +1,25 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ProjectBank.BusinessLogic.Features.Cards.Commands;
+using ProjectBank.BusinessLogic.Features.Currency;
 
 namespace ProjectBank.Presentation.Controllers
 {
     [Route("api/card")]
     [ApiController]
-    public class CardController(IMediator mediator) : ControllerBase
+    public class CardController(IMediator mediator, IGetNewestCurrency newestCurrency) : ControllerBase
     {
         [HttpPost]
         public async Task<IActionResult> Post(AddCardCommand cardCommand)
         {
             var res = await mediator.Send(cardCommand);
             return Ok(res);
+        }
+        [HttpGet]
+        public string Get()
+        {
+            var res = newestCurrency.GetFromApi();
+            return res;
         }
     }
 }
