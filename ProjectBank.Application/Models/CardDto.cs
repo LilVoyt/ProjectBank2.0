b8d8fde6d6d4c0d098f0b16df125965a0d1d9f8f
Voytanovskyy <olegvoyt6@gmail.com>
2024-10-19 13:56:07 +0300

    change with card and jwt update

--- ProjectBank.Application/Models/CardDto.cs
+++ ProjectBank.Application/Models/CardDto.cs
@@ -5,9 +5,9 @@
         public Guid Id { get; set; }
         public string NumberCard { get; set; }
         public string CardName { get; set; }
-        public int Pincode { get; set; }
+        public string Pincode { get; set; }
         public DateTime Data { get; set; }
-        public int CVV { get; set; }
+        public string CVV { get; set; }
         public double Balance { get; set; }
     }
 }
